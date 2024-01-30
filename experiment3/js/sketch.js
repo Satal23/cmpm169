24fb2dded0c8a5b9074e610cf3df9d148c571549
Satal23 <115119495+Satal23@users.noreply.github.com>
2024-01-29 23:57:06 -0800

    Experiment 3

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,67 +1,62 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let rows = 100;
+let cols = 50;
+let cubeSize = 6;
+let cubes = [];
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  setupCubes();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+function setupCubes() {
+  //grid for cubes
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      cubes.push(new Cube(i * cubeSize * 2, j * cubeSize * 2));
+    }
+  }
+}
+
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(220);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  for (let cube of cubes) {
+    cube.move();
+    cube.display();
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+class Cube {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.zOffset = random(1000);
+  }
+
+  move() {
+    //perlin noise to move cubes up and down
+    let noiseVal = noise(this.x * 0.01, this.y * 0.01, frameCount * 0.01);
+    this.z = map(noiseVal, 0, 1, -100, 100);
+
+    //find mouse proximity and change the height of the cubes depending on the mouse position
+    let mouseDist = dist(this.x, this.y, mouseX - width / 2, mouseY - height / 2);
+    let zChange = map(-mouseDist, 0, sqrt(sq(width) + sq(height)), 0, 100);
+    this.z += zChange;
+  }
+
+  display() {
+
+    push();
+    translate(this.x - width / 2, this.y - height / 2, this.z);
+    //change color depending on mouse position
+    fill(mouseX, mouseY, (mouseX-mouseY)*2);
+    box(cubeSize);
+    pop();
+  }
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  setupCubes();
+}
+s
